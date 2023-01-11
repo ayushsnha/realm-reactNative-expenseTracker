e06b9a2bdddac8cbfe702acff323ef21b87e04d5
ayushsnha <ayush_snha@rediffmail.com>
2023-01-11 13:39:50 +0530

    navigation init

--- App.tsx
+++ App.tsx
@@ -1,30 +1,59 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const Home = ({ navigation }: any) => (
+  <ScrollView
+    contentInsetAdjustmentBehavior="automatic"
+    contentContainerStyle={styles.backgroundStyle}>
+    <View>
+      <Text>Hello</Text>
+      <Button
+        title="Go to Expenses"
+        onPress={() => navigation.navigate('Expenses')}
+      />
+    </View>
+  </ScrollView>
+);
+
+const Expenses = () => (
+  <ScrollView
+    contentInsetAdjustmentBehavior="automatic"
+    contentContainerStyle={styles.backgroundStyle}>
+    <View>
+      <Text>Expenses</Text>
+    </View>
+  </ScrollView>
+);
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.backgroundStyle}>
-      <StatusBar />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View>
-          <Text>Hello</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: 'Home' }}
+        />
+        <Stack.Screen
+          name="Expenses"
+          component={Expenses}
+          options={{ title: 'Expenses' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
   backgroundStyle: {
-    backgroundColor: 'red',
+    backgroundColor: '#EEF8F7',
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   sectionContainer: {
     marginTop: 32,
--- android/app/src/main/java/com/expensetracker/MainActivity.java
+++ android/app/src/main/java/com/expensetracker/MainActivity.java
@@ -3,6 +3,7 @@ package com.expensetracker;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -23,6 +24,14 @@ public class MainActivity extends ReactActivity {
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new MainActivityDelegate(this, getMainComponentName());
+  }
+
+    /**
+   * For React Navigation
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   public static class MainActivityDelegate extends ReactActivityDelegate {
